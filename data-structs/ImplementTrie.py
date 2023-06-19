bf8805a7814e235dc21ba299ee6db8ebf1fa7d02
albert <aalbertuntung@gmail.com>
2023-06-19 15:51:50 -0700

    Create ImplementTrie.py

---
+++ data-structs/ImplementTrie.py
@@ -0,0 +1,62 @@
+class TrieNode:
+    def __init__(self, char):
+        self.char = char
+        self.children = {}
+        self.isEndOfWord = False
+
+class Trie:
+
+    def __init__(self):
+        self.children = {}
+
+
+    def getInitialNode(self, word: str, shouldCreate: bool) -> TrieNode:
+        if not word: return None
+        prevNode = self.children.get(word[0], None)
+        if prevNode is None and shouldCreate:
+            prevNode = TrieNode(word[0])
+            self.children[word[0]] = prevNode
+        return prevNode
+            
+
+    def insert(self, word: str) -> None:
+        if not word: return
+        prevNode = self.getInitialNode(word, True)
+        for index in range(1, len(word)):
+            char = word[index]
+            childNode = prevNode.children.get(char, None)
+            if childNode is None:
+                childNode = TrieNode(char)
+                prevNode.children[char] = childNode
+            prevNode = childNode
+        prevNode.isEndOfWord = True
+            
+
+    def search(self, word: str) -> bool:
+        if not word: return False
+        prevNode = self.getInitialNode(word, False)
+        if prevNode is None: return False
+        for index in range(1, len(word)):
+            char = word[index]
+            childNode = prevNode.children.get(char, None)
+            if childNode is None: return False
+            prevNode = childNode
+        return prevNode.isEndOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        if not prefix: return False
+        prevNode = self.getInitialNode(prefix, False)
+        if prevNode is None: return False
+        for index in range(1, len(prefix)):
+            char = prefix[index]
+            childNode = prevNode.children.get(char, None)
+            if childNode is None: return False
+            prevNode = childNode
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
