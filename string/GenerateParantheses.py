3f17f432cb865ddf6bea0078334be356de06367d
albert <aalbertuntung@gmail.com>
2023-07-04 10:56:17 -0700

    DFSes

--- string/GenerateParantheses.py
+++ string/GenerateParantheses.py
@@ -12,3 +12,19 @@ class Solution:
             self.helper(n, {"o": counts["o"] + 1, "c": counts["c"]}, res, strSoFar + "(")
         if counts["o"] > 0 and counts["o"] > counts["c"]: # We can also close
             self.helper(n, {"o": counts["o"], "c": counts["c"] + 1}, res, strSoFar + ")")
+
+## With no dict
+class Solution2:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        self.helper(n, 0, 0, res, "")
+        return res
+
+    def helper(self, n, left, right, res, strSoFar):
+        if left == right and left == n:
+            res.append(strSoFar)
+            return
+        if left < n: # We can still open
+            self.helper(n, left + 1, right, res, strSoFar + "(")
+        if left > 0 and left > right: # We can also close
+            self.helper(n, left, right + 1, res, strSoFar + ")")
